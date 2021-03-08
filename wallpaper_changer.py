a0350551b3a9268c8aa5f71fc32b96e8ae78f34d
Billy <wsiegener@gmail.com>
2021-03-08 00:17:38 -0500

    initial commit

---
+++ wallpaper_changer.py
@@ -0,0 +1,34 @@
+import requests
+import ctypes
+import datetime
+import time
+import os
+
+url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/5424x5424.jpg"       # GOES-EAST Full
+# url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/5000x3000.jpg"  # Continenetal US
+
+img_url = r"C:\PATH\TO\SCRIPT\latest.jpg"
+
+def update_image():
+    with requests.get(url) as req:
+        with open("latest.jpg", "wb") as img:
+            img.write(req.content)
+
+    # set image as background
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, img_url, 0)
+
+    print("Updating image:", str(datetime.datetime.now()))
+
+
+if __name__ == "__main__":
+    # delete previous image
+    if os.path.exists("latest.jpg"):
+        os.remove("latest.jpg")
+
+    while 1:
+        update_image()
+
+        # 10 min delay
+        time.sleep(600)
+
+# end file
